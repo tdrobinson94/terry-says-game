a2b35ed8a9eb9faf31c45ec00bbe634ec7501ebc
tdrobinson94 <tdrobinson94@gmail.com>
2016-07-18 15:45:44 -0400

    just playing around

--- src/js/main.js
+++ src/js/main.js
@@ -5,26 +5,23 @@ import {Level} from './classes/level';
 
 let level = new Level();
 let newGame = new Game();
+let audio = $('audio')[0];
 
 $('.red').on('click', function(){
   newGame.Select('red');
-  // console.log(newGame.clickedColor);
 });
 
 $('.blue').on('click', function(){
   newGame.Select('blue');
-  // console.log(newGame.clickedColor);
 });
 
 $('.green').on('click', function(){
   newGame.Select('green');
-  // console.log(newGame.clickedColor);
 });
 
 
 $('.yellow').on('click', function(){
   newGame.Select('yellow');
-  // console.log(newGame.clickedColor);
 });
 
 let color = newGame.ClickedColor;
